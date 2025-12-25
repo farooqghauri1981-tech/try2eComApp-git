65033212f629a06a3c584adf3205dc92b7c1ec5e
GhauriChishti <123356861+GhauriChishti@users.noreply.github.com>
2025-12-25 14:46:11 +0500

    25122025 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:myapp/screens/auth-ui/sign_up_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 191, 176, 217)),
-      ),
-      home: const SignUpScreen(),
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Firebase Initialized'))),
     );
   }
 }
